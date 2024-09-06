48d526cc2d0cb5cca102646b92ef06989c3a2635
vanx <#vanxxxx>
2024-09-06 15:40:17 +0300

    smart mob spawner system v1

---
+++ Content.Server/Spawners/Components/SmartMobSpawnerComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Server.Spawners.Components;
+
+/// <summary>
+/// Spawns a MobPrototype entity and stores its UID.
+/// Starts a respawn timer once the entity was deleted.
+/// </summary>
+[RegisterComponent]
+public sealed partial class SmartMobSpawnerComponent : Component, ISerializationHooks
+{
+    [DataField(required: true)]
+    public EntProtoId MobPrototype = string.Empty;
+
+    [DataField]
+    public bool SpawnMobOnInit = true; // If false, will start a timer instead.
+
+    [DataField]
+    public int MinRespawnTimeSeconds = 60;
+
+    [DataField]
+    public int MaxRespawnTimeSeconds = 120;
+
+    [DataField, ViewVariables(VVAccess.ReadOnly)]
+    public int RespawnTime;
+
+    [DataField, ViewVariables(VVAccess.ReadOnly)]
+    public int RespawnTimeLeft;
+
+    [DataField, ViewVariables(VVAccess.ReadOnly)]
+    public EntityUid SpawnedMob = EntityUid.Invalid;
+
+    public CancellationTokenSource? TokenSource;
+
+    void ISerializationHooks.AfterDeserialization()
+    {
+        if (MinRespawnTimeSeconds > MaxRespawnTimeSeconds)
+            throw new ArgumentException("MaxRespawnTimeSeconds can't be lower than MinRespawnTimeSeconds!");
+    }
+}
---
+++ Content.Server/Spawners/EntitySystems/SmartMobSpawnerSystem.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading;
+using Content.Server.Spawners.Components;
+using Robust.Shared.Random;
+
+namespace Content.Server.Spawners.EntitySystems;
+
+public sealed class SmartMobSpawnerSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<SmartMobSpawnerComponent, ComponentInit>(OnSpawnerInit);
+        SubscribeLocalEvent<SmartMobSpawnerComponent, ComponentShutdown>(OnSpawnerShutdown);
+        SubscribeLocalEvent<SmartMobSpawnerSpawnedComponent, ComponentShutdown>(OnMobCompShutdown);
+    }
+
+    private void OnSpawnerInit(EntityUid uid, SmartMobSpawnerComponent component, ComponentInit args)
+    {
+        if (component.SpawnMobOnInit)
+            SpawnMob(uid, component);
+        else
+            StartRespawnTimer(uid);
+    }
+
+    private void OnSpawnerShutdown(EntityUid uid, SmartMobSpawnerComponent component, ComponentShutdown args)
+    {
+        component.TokenSource?.Cancel();
+    }
+
+    private void OnMobCompShutdown(EntityUid uid, SmartMobSpawnerSpawnedComponent component, ComponentShutdown args)
+    {
+        var spawner = component.SpawnedBy;
+
+        if (Deleted(spawner))
+        {
+            Log.Error($"Spawned mob {uid} was removed but spawner {spawner} no longer exists!");
+            return;
+        }
+
+        StartRespawnTimer(spawner);
+    }
+
+    private void StartRespawnTimer(EntityUid uid)
+    {
+        if (!TryComp<SmartMobSpawnerComponent>(uid, out var component))
+        {
+            Log.Error($"Tried to start a timer on an entity {uid} that has no SmartMobSpawnerComponent!");
+            return;
+        }
+
+        component.TokenSource?.Cancel();
+        component.TokenSource = new CancellationTokenSource();
+        component.RespawnTime = _random.Next(component.MinRespawnTimeSeconds, component.MaxRespawnTimeSeconds);
+
+        Log.Debug($"Mob spawner {uid} started a {component.RespawnTime} seconds respawn timer for a {component.MobPrototype}");
+        uid.SpawnTimer(TimeSpan.FromSeconds(component.RespawnTime), () => OnTimerFired(uid, component), component.TokenSource.Token);
+
+    }
+
+    private void OnTimerFired(EntityUid uid, SmartMobSpawnerComponent component)
+    {
+        if (Exists(component.SpawnedMob))
+        {
+            Log.Debug($"Spawner {uid} is spawning {component.MobPrototype} while {component.SpawnedMob} still exists.");
+
+            // Can only happen if the SmartMobSpawnerSpawnedComponent
+            // Was removed from the entity without deleting the entity itself.
+            // In which case it was probably intended to allow spawning a new mob.
+            // Uncomment return if it ever wasn't.
+
+            //return;
+        }
+
+        SpawnMob(uid, component);
+    }
+
+    private void SpawnMob(EntityUid uid, SmartMobSpawnerComponent component)
+    {
+        var coordinates = Transform(uid).Coordinates;
+        component.SpawnedMob = SpawnAtPosition(component.MobPrototype, coordinates);
+        EnsureComp<SmartMobSpawnerSpawnedComponent>(component.SpawnedMob, out var spawnedMobComp);
+        spawnedMobComp.SpawnedBy = uid;
+    }
+}
