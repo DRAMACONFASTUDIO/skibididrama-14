b195395adaabf014a7322f3ef476a557a9869f60
vanx <#vanxxxx>
2024-09-06 21:11:48 +0300

    evaporate blood

--- Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
+++ Content.Shared/Fluids/SharedPuddleSystem.Evaporation.cs
@@ -8,7 +8,19 @@ public abstract partial class SharedPuddleSystem
     [ValidatePrototypeId<ReagentPrototype>]
     private const string Water = "Water";
 
-    public static readonly string[] EvaporationReagents = [Water];
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string Blood = "Blood";
+
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string InsectBlood = "InsectBlood";
+
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string Vomit = "Vomit";
+
+    [ValidatePrototypeId<ReagentPrototype>]
+    private const string CopperBlood = "CopperBlood";
+
+    public static readonly string[] EvaporationReagents = [Water, Blood, InsectBlood, Vomit, CopperBlood];
 
     public bool CanFullyEvaporate(Solution solution)
     {
